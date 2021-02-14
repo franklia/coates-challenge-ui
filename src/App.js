e25cb75d04533b65fa06062b7e76ec729510f830
Frank Liardet <frank.liardet@gmail.com>
2021-02-15 10:14:50 +1100

    Removed console.log

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
 
   useEffect(() => {
     const values = getTemperatures();
-    console.log('temp values in useEffect: ', values);
   }, []);
 
   const getTemperatures = () => {
